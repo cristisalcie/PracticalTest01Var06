b69f9c8254bb68793c3d901b6ecc33c8743b5ebd
cristisalcie <cristisalcie@yahoo.com>
2022-04-11 10:32:51 +0300

    Completed task B.1

---
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var06/Constants.java
@@ -0,0 +1,5 @@
+package ro.pub.cs.systems.eim.practicaltest01var06;
+
+public interface Constants {
+    final public static String TAG = "practicaltest01var06";
+}
--- app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var06/PracticalTest01Var06MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var06/PracticalTest01Var06MainActivity.java
@@ -3,8 +3,10 @@ package ro.pub.cs.systems.eim.practicaltest01var06;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 
 import java.util.Random;
@@ -17,19 +19,80 @@ public class PracticalTest01Var06MainActivity extends AppCompatActivity {
 
         @Override
         public void onClick(View view) {
+            EditText firstEditText = (EditText) findViewById(R.id.first_edit_text);
+            EditText secondEditText = (EditText) findViewById(R.id.second_edit_text);
+            EditText thirdEditText = (EditText) findViewById(R.id.third_edit_text);
+
+            CheckBox firstCheckbox = (CheckBox) findViewById(R.id.hold1_checkbox);
+            CheckBox secondCheckbox = (CheckBox) findViewById(R.id.hold2_checkbox);
+            CheckBox thirdCheckbox = (CheckBox) findViewById(R.id.hold3_checkbox);
+
             if (view.getId() == R.id.play_button) {
-                int randomNum = random.nextInt(4);
-                switch (randomNum) {
-                    case 0:  // star
-
-                        break;
-                    case 1:
-                        break;
-                    case 2:
-                        break;
-                    case 3:
-                        break;
-
+                {
+                    int randomNum = random.nextInt(4);
+                    String tmpText = null;
+                    switch (randomNum) {
+                        case 0:  // star
+                            tmpText = "*";
+                            break;
+                        case 1:
+                            tmpText = "1";
+                            break;
+                        case 2:
+                            tmpText = "2";
+                            break;
+                        case 3:
+                            tmpText = "3";
+                            break;
+                    }
+                    if (!firstCheckbox.isChecked()) {
+                        firstEditText.setText(tmpText);
+                    }
+                    Log.d(Constants.TAG, "Number 1: " + tmpText);
+                }
+                {
+                    int randomNum = random.nextInt(4);
+                    String tmpText = null;
+                    switch (randomNum) {
+                        case 0:  // star
+                            tmpText = "*";
+                            break;
+                        case 1:
+                            tmpText = "1";
+                            break;
+                        case 2:
+                            tmpText = "2";
+                            break;
+                        case 3:
+                            tmpText = "3";
+                            break;
+                    }
+                    if (!secondCheckbox.isChecked()) {
+                        secondEditText.setText(tmpText);
+                    }
+                    Log.d(Constants.TAG, "Number 2: " + tmpText);
+                }
+                {
+                    int randomNum = random.nextInt(4);
+                    String tmpText = null;
+                    switch (randomNum) {
+                        case 0:  // star
+                            tmpText = "*";
+                            break;
+                        case 1:
+                            tmpText = "1";
+                            break;
+                        case 2:
+                            tmpText = "2";
+                            break;
+                        case 3:
+                            tmpText = "3";
+                            break;
+                    }
+                    if (!thirdCheckbox.isChecked()) {
+                        thirdEditText.setText(tmpText);
+                    }
+                    Log.d(Constants.TAG, "Number 3: " + tmpText);
                 }
             }
         }
