b6ae0ec33ab2019529056df657aa34a02581f57d
cristisalcie <cristisalcie@yahoo.com>
2022-04-11 10:25:14 +0300

    completed A.2

--- app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var06/PracticalTest01Var06MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var06/PracticalTest01Var06MainActivity.java
@@ -3,12 +3,45 @@ package ro.pub.cs.systems.eim.practicaltest01var06;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.Random;
 
 public class PracticalTest01Var06MainActivity extends AppCompatActivity {
+    final private Random random = new Random();
+    private ButtonClickListener buttonClickListener = new ButtonClickListener();
+
+    private class ButtonClickListener implements Button.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            if (view.getId() == R.id.play_button) {
+                int randomNum = random.nextInt(4);
+                switch (randomNum) {
+                    case 0:  // star
+
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+
+                }
+            }
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practical_test01_var06_main);
+
+        Button playButton = (Button) findViewById(R.id.play_button);
+        playButton.setOnClickListener(buttonClickListener);
     }
 }
